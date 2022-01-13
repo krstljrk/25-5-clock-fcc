a1052f3f362b708387eca84dd06e6b4571e576cd
krstjrk <kristel.juurik@gmail.com>
2022-01-13 18:47:43 +0200

    first setup

---
+++ src/components/25+5-clock.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../stylesheets/styles.scss';
+
+export default class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="">
+
+            </div>
+        )
+    }
+}
